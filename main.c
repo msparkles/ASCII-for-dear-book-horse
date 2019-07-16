662a5969d1d513566eae14ad2f5d4ec15d479f55
Mu_guang_138 <muguang138@gmail.com>
2019-07-17 04:13:40 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#define twi 1
+#include <stdio.h>
+#define print(void){char                love='T',TWILY[5] [5]={{31               ,twi<<2, twi<<2,4,twi<<2}             ,{(twi<<4)           +1, 1+(twi\
+<<4),21,        21,10}, {31         ,twi<<2,4,            twi<<2,31},         {twi <<4,             16,twi<<4,         twi<<4,31}     ,{17,10,twi<<2,\
+twi <<2,        twi << 2}},       books =twi*               0,kjjkjkkj,     __ ; STORE:               while(twi)       {if (books>=5)break;\
+kjjkjkkj =twi>>1; BUYBOOKS:       while(twi){               if(kjjkjkkj     >=5)break;                char b=(16)      ;__= twi << 8;\
+PAYFORBOOKS:while(twi) {if(       __<(twi<<3)               )goto  PAY;    else;break;                PAY:printf(      "%c",TWILY[kjjkjkkj]\
+[books]         &b?love:' '        );b>>=1;__               +=twi;goto     PAYFORBOOKS;               }BUY:love+=      (love=='T' ? 3:love=='W'?242\
+:love==         'I'?3: love        =='L' ?13 :             251);printf       ("    ");                kjjkjkkj=        kjjkjkkj +twi; goto BUYBOOKS;}\
+BACKTOSTORE: printf ("\n")           ;books+=twi;goto STORE;}printf(         "\nBook Horse is the Best Horse\n"        );system              ("Pause");}
+int main(){print();}
